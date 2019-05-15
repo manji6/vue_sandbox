67280c313ba334de199b33a2b7deb1d6821f79bc
manji6 <ryosuke.sawada@gmail.com>
2019-05-15 21:37:58 +0900

    change "dist" to "docs"

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -48,7 +48,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../docs'), //@MEMO changed "dist" to "docs" because github Pages supported only "docs" directory.
     assetsSubDirectory: 'static',
     assetsPublicPath: '',   //@MEMO removed "/" because fiexed path can't work in githubPages
 
