15e20af17010af7493c5c6d72e063aca5d045fef
manji6 <ryosuke.sawada@gmail.com>
2019-05-15 22:15:39 +0900

    added page1/page2 routing and change router mode(use HistoryAPI)

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
+
+// added
+import page1 from '@/components/page1'
+import page2 from '@/components/page2'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',  // これでハッシュがつかなくなる
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/page1',
+      name: 'page1',
+      component: page1
+    },
+    {
+      path: '/page2',
+      component: page2
     }
   ]
 })
